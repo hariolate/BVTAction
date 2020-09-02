4de4758f6662460b9ada91bc29bd28f5d55d52f9
huang825172 <1048035187@qq.com>
2020-09-03 00:55:01 +0800

    Add: Home, Login, Signup

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,13 +1,90 @@
 <template>
-    <h1>Signup</h1>
+    <div>
+        <div>
+            <b-navbar
+                    type="light"
+                    variant="light"
+                    id="nav">
+                <b-navbar-nav class="ml-auto" >
+                    <b-nav-item href="#/">Home</b-nav-item>
+                    <b-nav-item href="#/login">Login</b-nav-item>
+                </b-navbar-nav>
+            </b-navbar>
+        </div>
+        <b-img
+                center
+                :src="logoImg"
+                alt="UNO Online"
+                id="logo"
+        ></b-img>
+        <b-form id="form">
+            <b-form-group
+                    id="emailInputGroup"
+                    label="Email address:"
+                    label-for="emailInput">
+                <b-form-input
+                        id="emailInput"
+                        v-model="form.email"
+                        type="email"
+                        required
+                        placeholder="Enter email"
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                    id="passwordInputGroup"
+                    label="Password:"
+                    label-for="passwordInput">
+                <b-form-input
+                        id="passwordInput"
+                        v-model="form.password"
+                        type="password"
+                        required
+                        placeholder="Enter password"
+                ></b-form-input>
+            </b-form-group>
+            <b-button
+                    type="submit"
+                    variant="primary"
+                    id="button">
+                Sign up
+            </b-button>
+        </b-form>
+    </div>
 </template>
 
 <script>
+    import common from "../common";
+
     export default {
-        name: "Signup"
+        name: "Signup",
+        data() {
+            return {
+                logoImg: common.logoImgSrc,
+                form: {
+                    email: '',
+                    password: '',
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    #nav {
+        border-bottom: solid lightgray 1px;
+    }
+    #logo {
+        margin-top: 2vh;
+        max-height: 300px;
+    }
+    #form {
+        width: 50%;
+        margin-left: 25%;
+        margin-top: 3vh;
+    }
+    #button {
+        width: 80%;
+        margin-left: 10%;
+        margin-top: 1vh;
+    }
 </style>
